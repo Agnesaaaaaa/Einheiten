bb92bbf5313715d58c7464b1369e3d8d028bfa53
Agnesaaaaaa <157700222+Agnesaaaaaa@users.noreply.github.com>
2024-04-03 20:19:15 +0200

    Update Masseinheiten.py

--- Masseinheiten.py
+++ Masseinheiten.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-formatted_text = st.markdown('<h1 style="color: pink; border-bottom: 2px solid lightpink;">Masseinheiten</h1>', unsafe_allow_html=True)
+formatted_text = st.markdown('<h1 style="color: blue; border-bottom: 2px solid lightpink;">Masseinheiten</h1>', unsafe_allow_html=True)
 def main():
     st.title('Einheiten Umrechner')
 
@@ -16,16 +16,16 @@ def main():
         'Gramm': 1
     }
 
-    # Eingabefeld für Benutzer
+ 
     input_value = st.number_input(f'Gib den Wert in {input_unit} ein:')
 
-    # Ziel Einheit
+
     output_unit = st.selectbox('Wähle Ziel Einheit:', ['Meter', 'Kilometer', 'Liter', 'Gramm'])
 
-    # Umrechnung
+    
     output_value = input_value * (unit_factors[input_unit] / unit_factors[output_unit])
 
-    # Ausgabe des Ergebnisses
+    
     st.write(f'Das Ergebnis ist {output_value} {output_unit}.')
 
 if __name__ == '__main__':
