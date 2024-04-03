aedbc7587cc29bad222df65163731da2ea6cb870
Agnesaaaaaa <157700222+Agnesaaaaaa@users.noreply.github.com>
2024-04-03 19:53:48 +0200

    Add files via upload

---
+++ Masseinheiten.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+formatted_text = st.markdown('<h1 style="color: pink; border-bottom: 2px solid lightpink;">Masseinheiten</h1>', unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
